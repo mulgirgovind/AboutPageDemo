688e0cc8be4264951850538b1e72b7c7b49ea8cf
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-12-11 12:26:41 +0530

    - Setup app to show app name and its current version - Make shared scheme

--- AboutPageDemo/ViewController.swift
+++ AboutPageDemo/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appCurrentVersionLabel: UILabel!
+    @IBOutlet weak var appNameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let appName = Bundle.main.infoDictionary!["CFBundleName"]!
+        let currentVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"]!
+
+        appNameLabel.text = "App name: \(appName)"
+        appCurrentVersionLabel.text = "Current version: \(currentVersion)"
     }
-
-
 }
-
